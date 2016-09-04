ff3e4f3ab4d6619bf0708b4a94e56f8ae3403442
yutannihilation <yutani.ini@gmail.com>
2016-09-04 13:49:18 +0900

    fix documentations

--- R/http_cat.R
+++ R/http_cat.R
@@ -1,14 +1,18 @@
 #' Display Cats For Status Code
 #'
+#' @name http_cat
+#' @param x status code number or response object
+#'
 #' @export
-http_cat <- function(code) UseMethod("http_cat")
+http_cat <- function(x) UseMethod("http_cat")
 
+#' @rdname http_cat
 #' @export
-http_cat.default <- function(code) {
+http_cat.default <- function(x) {
   shiny::runApp(
     shiny::shinyApp(
       shiny::fluidPage(
-        shiny::img(src=sprintf("https://http.cat/%d", code)),
+        shiny::img(src=sprintf("https://http.cat/%d", x)),
         shiny::actionButton("done", "I got it.")
       ),
       function(input, output) {
@@ -21,14 +25,16 @@ http_cat.default <- function(code) {
     ))
 }
 
+#' @rdname http_cat
 #' @export
-http_cat.response <- function(res) {
-  code <- httr::status_code(res)
-  http_cat.default(code)
+http_cat.response <- function(x) {
+  code <- httr::status_code(x)
+  http_cat.default(x)
 }
 
+#' @inheritParams print
 #' @export
-print.response <- function(res) {
-  http_cat(res)
-  httr:::print.response(res)
+print.response <- function(x, ...) {
+  http_cat(x)
+  httr:::print.response(x, ...)
 }
